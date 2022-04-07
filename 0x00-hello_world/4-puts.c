ca3306b448bd2f9c8794acdc67389bebfc2c149c
Munaoz <munaozzoude@gmail.com>
2022-04-07 08:08:14 -0700

    vex

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,12 @@
 #include <stdio.h> 
 
- /** 
-  *
-  *   * Description: main - Prints out a string to stdout. 
-  *
-  *     * Return: 0 if success. 
-  *
-  *       */ 
+/**
+ * Description:main  - Prints out a string to stdout
+ * Return : 0 if sucess 
+ */
 
- int main(void) 
-
-	 { 
-
-		  puts("\"Programming is like building a multilingual puzzle"); 
-
-		   return (0); 
-
-		    }
-
-
+int main(void)
+{
+puts("/"Programming is like bulding a multilingual puzzle");
+return (0);
+}
