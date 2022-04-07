fdfa3271b06a4d1ee0275dc7c9a9e91336a99469
Munaoz <munaozzoude@gmail.com>
2022-04-07 08:11:17 -0700

    t

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,17 @@
 #include <stdio.h> 
 
-/**
- * Description:main  - Prints out a string to stdout
- * Return : 0 if sucess 
- */
+ /** 
+  *
+  *   * Description: main - Prints out a string to stdout. 
+  *
+  *     * Return: 0 if success. 
+  *
+  *       */ 
 
-int main(void)
-{
-puts("/"Programming is like bulding a multilingual puzzle");
-return (0);
-}
+ int main(void) 
+ { 
+
+ Puts("\"Programming is like building a multilingual puzzle");
+
+ return (0);
+ }
