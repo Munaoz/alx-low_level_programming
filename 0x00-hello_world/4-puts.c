40f2c0afcc7ee4c0076d4af3d46da44061a04127
Munaoz <munaozzoude@gmail.com>
2022-04-07 08:15:22 -0700

    fro

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,9 +9,13 @@
   *       */ 
 
  int main(void) 
- { 
 
- Puts("\"Programming is like building a multilingual puzzle");
+	 { 
 
- return (0);
- }
+		  Puts("\"Programming is like building a multilingual puzzle"); 
+
+		   return (0); 
+
+		    }
+
+
