ba7e233478fc0e925a34e10a5bdf934240f71459
Munaoz <munaozzoude@gmail.com>
2022-04-07 11:55:00 -0700

    k

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  /** 
   *   * main - Entry point 
   *
-  *       * Return: Always 0 (Success) 
+  *     * Return: Always 0 (Success) 
   *
   */ 
 
@@ -13,6 +13,7 @@ int main(void)
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
 sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 return (1); 
+
 }
 
 
