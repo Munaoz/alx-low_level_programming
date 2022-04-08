e2ccf8d4980826f0ffe3a39d6bd10ba173e98180
Munaoz <munaozzoude@gmail.com>
2022-04-08 12:39:48 -0700

    k

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,8 +7,8 @@
 int main(void)
 {
 char alpha;
-for (alpha = 'a' ; alpha <='z' ; alpha++)
-if (alpha !='q' && alpha != 'e')
+for (alpha = 'a' ; alpha < ='z' ; alpha++)
+if (alpha ! ='q' && alpha ! = 'e')
 {
 putchar(alpha);
 }
